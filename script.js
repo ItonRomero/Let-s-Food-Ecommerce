85e94cd9e498b2d84498a53f0069f4e7d2531d65
ItonRomero <jaysonromero1218@gmail.com>
2022-10-25 23:07:54 +0800

    added media queries and hero section

--- script.js
+++ script.js
@@ -15,10 +15,7 @@ const navMenu = document.querySelector(".nav-menu");
 const menuIcon = document.querySelector(".menu-icon")
 
 menuIcon.addEventListener("click", function(){
-      if (navMenu.style.display === "none") {
-            navMenu.style.display = "flex";
-      } else {
-            navMenu.style.display = "none";
-      }
+      navMenu.classList.toggle("show-links");
+      menuIcon.style.zIndex = "100";
 })
 
