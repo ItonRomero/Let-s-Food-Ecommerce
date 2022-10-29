6ec1b5cea3012afc2c53a18e22394ccb3e2a2b9c
ItonRomero <jaysonromero1218@gmail.com>
2022-10-29 16:24:58 +0800

    still working OOP

--- script.js
+++ script.js
@@ -19,3 +19,63 @@ menuIcon.addEventListener("click", function(){
       menuIcon.style.zIndex = "100";
 })
 
+
+
+
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(
+    `Please check "${selection}" selector, no such element exists`
+  );
+}
+
+
+function Review(image, name, stars, comment) {
+      this.image = image;
+      this.name = name;
+      this.stars = stars;
+      this.comment = comment; 
+
+      this.card = getElement('.test')
+      this.displayReviews = this.displayReview.bind(this);
+      this.card.innerHTML = this.displayReviews;
+}
+
+
+
+
+
+ 
+ Review.prototype.displayReview = function (items) {
+       
+      this.card.innerHTML = items.map(function(item){
+              return `<div class="info flexitem">
+                     <div class="img">
+                             <img src="${item.image}" alt="">
+                         </div>
+                          <div class="name">
+                               <h5>${item.name}</h5>
+                               <ul class="flexitem">
+                                     <li${item.stars}</i></li>
+                                     <li><i class="fa-solid fa-star"></i></li>
+                                     <li><i class="fa-solid fa-star"></i></li>
+                                     <li><i class="fa-solid fa-star"></i></li>
+                                     <li><i class="fa-solid fa-star-half-stroke"></i></li>
+                               </ul>
+                          </div>
+                         </div>
+                         <div class="desc">
+                         <p>${item.comment}</p>
+                        </div>`  
+      }).join('')
+ }
+
+ 
+
+                     
+const firstReview = new Review("/images/review 1.png", "Jayson Romero", 5 , "teasdasdasdasdasdasdas");
+
+console.log(firstReview.displayReviews());
